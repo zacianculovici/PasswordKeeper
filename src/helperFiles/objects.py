ac746b418c86c639e317bd0fa7f2f0c4b17995b8
Zac I <zachary.ianculovici@gmail.com>
2026-09-04 11:51:38 +1000

    Added DangerousButton class to objects.py for red high-impact buttons

--- src/helperFiles/objects.py
+++ src/helperFiles/objects.py
@@ -1,4 +1,6 @@
 import hashlib
+from customtkinter import CTkButton
+import customtkinter
 
 class User:
     def __init__(self, username, password):
@@ -16,3 +18,23 @@ class NoAccountError(Exception):
     def __init__(self, message="User account does not exist. Please create a new account."):
         self.message = message
         super().__init__(self.message)
+
+class DangerousButton(CTkButton):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(
+            fg_color="transparent",
+            hover=False,
+            text_color="white",
+            border_color="red",
+            border_width=2,
+            corner_radius=8,
+        )
+        self.bind("<Enter>", lambda event: self.configure(fg_color="red"))
+        self.bind("<Leave>", lambda event: self.configure(fg_color="transparent"))
+
+if __name__ == "__main__":
+    app = customtkinter.CTk()
+    button = DangerousButton(app, text="Dangerous Action")
+    button.pack(pady=20, padx=20)
+    app.mainloop()
